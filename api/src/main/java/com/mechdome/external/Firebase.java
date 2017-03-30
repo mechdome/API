b3313ea1deb2c8c6a99a637d157f583577dd3b04
Mario Kosmiskas <md-mariok@users.noreply.github.com>
2017-03-30 16:34:39 -0700

    Added Apache 2.0 License

--- api/src/main/java/com/mechdome/external/Firebase.java
+++ api/src/main/java/com/mechdome/external/Firebase.java
@@ -1,10 +1,33 @@
+/*
+ * Copyright (C) 2017 MechDome - http://www.mechdome.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.mechdome.external;
 
 /**
+ * Provides access to the host OS's Firebase SDK
+ *
  * Created by Mario Kosmiskas on 3/29/17.
  */
 @SuppressWarnings("unused")
 public class Firebase {
+
+    /**
+     * Retrieves the token from the native Firebase SDK
+     * @return Firebase token
+     */
     @SuppressWarnings("unused")
     public static native String getToken();
 }
--- api/src/main/java/com/mechdome/view/HostOSView.java
+++ api/src/main/java/com/mechdome/view/HostOSView.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2017 MechDome - http://www.mechdome.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.mechdome.view;
 
 import android.graphics.Canvas;
@@ -14,6 +30,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * An Android view that delegates rendering to a native view in the host OS
+ *
  * Created by Mario Kosmiskas on 3/29/17.
  */
 public class HostOSView extends View {
@@ -129,6 +147,5 @@ public class HostOSView extends View {
         touchEvent(ev);
         return true;
     }
-
 
 }
